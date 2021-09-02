2554af16c489e71498e6feee8231d233c60da460
Kumar Sarvesh <70770975+kr-sarvesh@users.noreply.github.com>
2021-09-02 12:28:48 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,43 @@
+
+var readlineSync = require('readline-sync');
+var userName = readlineSync.question("what is your name buddy?\n")
+console.log("Welcome "+userName+" Let me see how well u know me? ")
+
+let score=0;
+function play(question, answer)
+{
+  var userAnswer= readlineSync.question(question);
+  if(userAnswer===answer)
+  {
+    score++;
+    console.log("right! now your current score is\n"+score)
+  }
+  else{
+    score--;
+    console.log("oops! your are wrong now your current score is\n"+score)
+  }
+  console.log("---------------------")
+}
+
+var questions = [
+  { question: "what is my name\n", 
+    answer:"sarvesh"},
+  { question:"what is my destination\n", 
+    answer:"patna"},
+    {
+    question:"what is my hobby\n", 
+    answer:"cricket"
+    },
+    {
+    question:"My favourite superHero would be\n",
+    answer:"Batman"
+    },
+    
+
+]
+
+for( i=0;i<questions.length;i++){
+  currentquestion= questions[i]
+  play(currentquestion.question, currentquestion.answer)
+
+}
